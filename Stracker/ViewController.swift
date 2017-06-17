0df28e053be1aa9b220bb4e7d25939e12b2e0149
George Madrid <gmadrid@gmail.com>
2017-06-17 09:53:08 -0400

    Initial Commit

---
+++ Stracker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Stracker
+//
+//  Created by George Madrid on 6/17/17.
+//  Copyright © 2017 George Madrid. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
